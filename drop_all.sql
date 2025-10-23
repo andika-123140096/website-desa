44645d71d2e67a70699cd6fd3e4d764af68b3375
Andika Dinata <a.notmuehehe@gmail.com>
2025-10-23 18:10:20 +0700

    LGTM

---
+++ drop_all.sql
@@ -0,0 +1,12 @@
+-- Drop all tables forcefully
+PRAGMA foreign_keys = OFF;
+
+DROP TABLE IF EXISTS tanggapan_aduan;
+DROP TABLE IF EXISTS aduan;
+DROP TABLE IF EXISTS surat_pbb;
+DROP TABLE IF EXISTS perangkat_desa;
+DROP TABLE IF EXISTS masyarakat;
+DROP TABLE IF EXISTS dusun;
+DROP TABLE IF EXISTS pengguna;
+
+PRAGMA foreign_keys = ON;
